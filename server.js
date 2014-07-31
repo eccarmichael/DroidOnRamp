d92849da3233dbc5c69310a91bba5f2de472e141
Elise Carmichael <ecarmichael@mobiquityinc.com>
2014-07-30 22:53:23 -0400

    port playing

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var fs = require("fs");
 var host = "127.0.0.1";
-var port = 3000;
+var port = 5000;
 var express = require("express");
 var thedata = require("./basicList.json");  // Initial data
 
