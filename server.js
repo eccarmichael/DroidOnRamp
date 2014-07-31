fb8e89a5df20eb802290fa397df17b56e2c8b47b
Elise Carmichael <ecarmichael@mobiquityinc.com>
2014-07-30 22:56:43 -0400

    more and more port stuff

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var fs = require("fs");
 var host = "127.0.0.1";
-var port = 5000;
+var port = process.env.PORT || 3000;
 var express = require("express");
 var thedata = require("./basicList.json");  // Initial data
 
